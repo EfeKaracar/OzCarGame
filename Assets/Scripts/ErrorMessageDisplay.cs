926ea429d36d0bc744cca3bcf51e2bb65167ef03
EfeKaracar <efekaracar@gmail.com>
2020-11-01 10:56:50 -0500

    project
    project

---
+++ Assets/Scripts/ErrorMessageDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorMessageDisplay : MonoBehaviour
+{
+    public Text msgTxt;
+    public Button dismissBtn;
+
+    public void DisplayMessage(string msgToShow,UnityEngine.Events.UnityAction actionOnDismiss)
+    {
+        msgTxt.text = msgToShow;
+        dismissBtn.onClick.AddListener(actionOnDismiss);
+    }
+}
